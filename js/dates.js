eaacf2654198ed21c65676c6e1c5378afdb8960c
gschiess <sch19005@byui.edu>
2022-01-08 21:01:42 -0700

    extra commit

--- js/dates.js
+++ js/dates.js
@@ -1,2 +1,6 @@
 const lastmod = document.querySelector('#lastmod')
 lastmod.textContent = `Last Updated: ${document.lastModified}`;
+
+let d = new Date();
+let fullyear = d.getFullYear();
+document.getElementById("thisyear").textContent = fullyear;
