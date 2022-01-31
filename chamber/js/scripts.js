b86afb46a96efd84fa79aaf86830bb844e3a8e62
gschiess <sch19005@byui.edu>
2022-01-30 23:53:34 -0700

    more changes

---
+++ chamber/js/scripts.js
@@ -0,0 +1,24 @@
+const lastmod = document.querySelector('#lastmod')
+lastmod.innerHTML = `<strong>Last Updated:</strong> ${document.lastModified}`;
+
+
+let d = new Date();
+let fullYear = d.getFullYear();
+document.getElementById("thisyear").textContent = fullYear;
+
+
+const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+const monthName = months[d.getMonth()]
+const dayName = days[d.getDay()]
+const date = d.getDate()
+const year = d.getFullYear()
+const currDate = `${dayName}, ${date} ${monthName} ${year}`
+document.getElementById("currentTime").textContent = currDate;
+
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
--- lesson3/js/dates.js
+++ lesson3/js/dates.js
@@ -2,5 +2,5 @@ const lastmod = document.querySelector('#lastmod')
 lastmod.innerHTML = `<strong>Last Updated:</strong> ${document.lastModified}`;
 
 let d = new Date();
-let fullyear = d.getFullYear();
-document.getElementById("thisyear").textContent = fullyear;
+let fullYear = d.getFullYear();
+document.getElementById("thisyear").textContent = fullYear;
