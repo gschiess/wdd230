dbaac01a0f3d35f9fa7704110203cf0623d05046
gschiess <sch19005@byui.edu>
2022-02-05 11:35:03 -0700

    fixed empty value in bom.html

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -4,18 +4,19 @@ const button = document.querySelector("button");
 button.onclick = function () {
   let scripture = input.value;
   input.value = "";
+  if (input.value != "") {
+    const scriptureList = document.createElement("li");
+    const scriptureText = document.createElement("span");
+    const addBtn = document.createElement("button");
 
-  const scriptureList = document.createElement("li");
-  const scriptureText = document.createElement("span");
-  const addBtn = document.createElement("button");
-
-  scriptureList.appendChild(scriptureText);
-  scriptureText.textContent = scripture;
-  scriptureList.appendChild(addBtn);
-  addBtn.textContent = 'Delete';
-  list.appendChild(scriptureList);
-  addBtn.onclick = function(e) {
+    scriptureList.appendChild(scriptureText);
+    scriptureText.textContent = scripture;
+    scriptureList.appendChild(addBtn);
+    addBtn.textContent = "Delete";
+    list.appendChild(scriptureList);
+    addBtn.onclick = function (e) {
       list.removeChild(scriptureList);
+    };
+    input.focus();
   }
-  input.focus();
 };
