ea4f7fb8a42fe99f2babd8c1b47ba72a9db80abc
gschiess <sch19005@byui.edu>
2022-02-05 11:30:25 -0700

    added bom.html

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,21 @@
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+button.onclick = function () {
+  let scripture = input.value;
+  input.value = "";
+
+  const scriptureList = document.createElement("li");
+  const scriptureText = document.createElement("span");
+  const addBtn = document.createElement("button");
+
+  scriptureList.appendChild(scriptureText);
+  scriptureText.textContent = scripture;
+  scriptureList.appendChild(addBtn);
+  addBtn.textContent = 'Delete';
+  list.appendChild(scriptureList);
+  addBtn.onclick = function(e) {
+      list.removeChild(scriptureList);
+  }
+  input.focus();
+};
