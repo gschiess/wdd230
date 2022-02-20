54a2db29da2fc3fcc835ae529354a4f2051fbe0b
gschiess <sch19005@byui.edu>
2022-02-19 23:47:25 -0700

    discover page

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -30,3 +30,27 @@ function showBanner() {
 if (dayName == ('Mon' || 'Tue')) {
     showBanner;
 }
+
+/**
+ * local storage stuff
+ */
+// initialize display elements
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".numVisits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+// check time between last visit
