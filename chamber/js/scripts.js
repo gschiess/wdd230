a5ba8791bea18cbe4acea62375099bb61e1d2032
gschiess <sch19005@byui.edu>
2022-02-26 22:45:55 -0700

    lasted visited date added

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -34,23 +34,19 @@ if (dayName == ('Mon' || 'Tue')) {
 /**
  * local storage stuff
  */
-// initialize display elements
-const todayDisplay = document.querySelector(".today");
-const visitsDisplay = document.querySelector(".numVisits");
-
-// get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
-
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit!`;
+// initialize display element
+const lastvisit_text = window.localStorage.getItem('lastvisit');
+if(lastvisit_text === undefined) {
+    lastvisit_text = new Date(Data.now());
 }
+const lastvisit = Date.parse(lastvisit_text);
+const FACTOR = 1000 * 60 * 60 * 24;
 
-// increment the number of visits.
-numVisits++;
-// store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
 
-// check time between last visit
+let daysbetween = Date.now() - lastvisit;
+let numberOfDays = daysbetween / FACTOR;
+
+window.localStorage.setItem('lastvisit', new Date(Date.now()));
+
+const daysSinceVisit = Math.floor(numberOfDays);
+document.getElementById('lastvisit').textContent = daysSinceVisit;
