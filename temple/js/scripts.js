c7d9f6bef6eb1a9f2794a5b60825dc4d3b7b0e93
OnlyTheCheesiest <71902030+OnlyTheCheesiest@users.noreply.github.com>
2022-04-03 21:53:24 -0600

    changes

--- temple/js/scripts.js
+++ temple/js/scripts.js
@@ -1,13 +1,23 @@
+// Script for Navigation Button functionality
 function toggleMenu() {
-  document.getElementById("primaryNav").classList.toggle("open");
-  document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-const x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu;
+  document.getElementById("primary-nav").classList.toggle("open");
+  document.getElementById("hamburger-btn").classList.toggle("open");
+};
+let btnOne = document.getElementById('hamburger-btn');
+btnOne.onclick = toggleMenu;
 
 function toggleMenuTwo() {
-  document.getElementById("secondaryNav").classList.toggle("open");
-  document.getElementById("hamburgerBtnTwo").classList.toggle("open");
-}
-const y = document.getElementById('hamburgerBtnTwo');
-y.onclick = toggleMenuTwo;
+  document.getElementById("secondary-nav").classList.toggle("open");
+  document.getElementById("hamburger-btn-two").classList.toggle("open");
+};
+let btnTwo = document.getElementById('hamburger-btn-two');
+btnTwo.onclick = toggleMenuTwo;
+
+
+// Script for Footer and Date stuff
+const lastmod = document.querySelector('#last-mod')
+lastmod.innerHTML = `<strong>Last Updated:</strong> ${document.lastModified}`;
+
+let d = new Date();
+let fullyear = d.getFullYear();
+document.getElementById("this-year").textContent = fullyear;
